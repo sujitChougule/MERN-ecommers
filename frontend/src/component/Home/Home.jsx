feb6bb656bca10656fd42b590b67ff1c06c542d5
sujitChougule <sujitchougule2003@gmail.com>
2023-10-29 03:50:38 +0530

    Home page and product card created

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import { CgMouse } from "react-icons/cg";
+import "./Home.scss";
+import Product from "./Product";
+import MetaDate from "../layouts/MetaDate";
+const product = {
+  name: "Blue Tshirt",
+  images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+  price: 3000,
+  _id: "sujit",
+};
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <MetaDate title="ShopNest" />
+      <div className="banner">
+        <p>Welcome to ShopNest</p>
+        <h1>Your One-Stop Shop</h1>
+        <a href="#homeHeading" className="">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
+      <h2 className="homeHeading" id="homeHeading">
+        Featured Products
+      </h2>
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
