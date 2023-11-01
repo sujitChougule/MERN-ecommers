a70b1090e684983d90b9ad3b8b0b185f9d445b2a
sujitChougule <sujitchougule2003@gmail.com>
2023-11-01 22:57:20 +0530

    product review

---
+++ frontend/src/component/Product/ReviewCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import profilePng from "../images/profilePng.png";
+const ReviewCard = ({ review }) => {
+  // options for react stars
+  const options = {
+    edit: false,
+    color: "rgba(20, 20, 20, 0.1)",
+    activeColor: "tomato",
+    size: window.innerWidth < 600 ? 25 : 30,
+    value: review.rating,
+    isHalf: true,
+  };
+  return (
+    <div className="reviewCard">
+      <img src={profilePng} alt="User" />
+      <p>{review.name}</p>
+      <ReactStars {...options} />
+      <span>{review.comment}</span>
+    </div>
+  );
+};
+
+export default ReviewCard;
